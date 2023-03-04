e1c4d8786f3f30238cda4f6c25dd33fb5fd13785
ferpe22 <ferpereira22@gmail.com>
2023-03-03 23:42:52 -0300

    limpieza de codigo

--- JS/darkToggle.js
+++ JS/darkToggle.js
@@ -1,29 +1,3 @@
-// let btnToggle = document.getElementById("toggleMode")
-
-// if(localStorage.getItem("modoOscuro")){
-//     if((JSON.parse(localStorage.getItem("modoOscuro")) == true)){
-//         // btnToggle.innerText = `Light`
-//         // btnToggle.className = `btn btn-light`
-//     }
-// }else{
-//     localStorage.setItem("modoOscuro", false)
-// }
-
-// btnToggle.addEventListener("click", ()=>{
-//     document.body.classList.toggle("darkMode")
-
-//     if(JSON.parse(localStorage.getItem("modoOscuro")) == false){
-//         // btnToggle.innerText = `Light`
-//         // btnToggle.className = `btn btn-light`
-//         localStorage.setItem("modoOscuro", true)
-//     }else{
-//         // btnToggle.innerText = `Dark`
-//         // btnToggle.className = `btn btn-dark`
-//         localStorage.setItem("modoOscuro", false)
-//     }
-// })
-
-
 let btnToggle = document.getElementById("toggleMode")
 const isDarkStorageOn = localStorage.getItem("modoOscuro") === "true"
 
