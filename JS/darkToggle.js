1b3827d2cbe2da1c4ee964f745849f01cb21ee72
ferpe22 <ferpereira22@gmail.com>
2023-03-03 23:37:39 -0300

    arreglo de dark mode y cambio de nombre de fotos

--- JS/darkToggle.js
+++ JS/darkToggle.js
@@ -1,24 +1,41 @@
-let btnToggle = document.getElementById("toggleMode")
+// let btnToggle = document.getElementById("toggleMode")
 
-if(localStorage.getItem("modoOscuro")){
-    if((JSON.parse(localStorage.getItem("modoOscuro")) == true)){
-        // btnToggle.innerText = `Light`
-        // btnToggle.className = `btn btn-light`
-    }
-}else{
-    localStorage.setItem("modoOscuro", false)
+// if(localStorage.getItem("modoOscuro")){
+//     if((JSON.parse(localStorage.getItem("modoOscuro")) == true)){
+//         // btnToggle.innerText = `Light`
+//         // btnToggle.className = `btn btn-light`
+//     }
+// }else{
+//     localStorage.setItem("modoOscuro", false)
+// }
+
+// btnToggle.addEventListener("click", ()=>{
+//     document.body.classList.toggle("darkMode")
+
+//     if(JSON.parse(localStorage.getItem("modoOscuro")) == false){
+//         // btnToggle.innerText = `Light`
+//         // btnToggle.className = `btn btn-light`
+//         localStorage.setItem("modoOscuro", true)
+//     }else{
+//         // btnToggle.innerText = `Dark`
+//         // btnToggle.className = `btn btn-dark`
+//         localStorage.setItem("modoOscuro", false)
+//     }
+// })
+
+
+let btnToggle = document.getElementById("toggleMode")
+const isDarkStorageOn = localStorage.getItem("modoOscuro") === "true"
+
+if (isDarkStorageOn) {
+    document.body.classList.add("darkMode")
+    btnToggle.checked = true
 }
 
-btnToggle.addEventListener("click", ()=>{
+btnToggle.addEventListener("click", () => {
+    const isDarkStorageOn = localStorage.getItem("modoOscuro") === "true"
+    console.log(isDarkStorageOn)
     document.body.classList.toggle("darkMode")
-
-    if(JSON.parse(localStorage.getItem("modoOscuro")) == false){
-        // btnToggle.innerText = `Light`
-        // btnToggle.className = `btn btn-light`
-        localStorage.setItem("modoOscuro", true)
-    }else{
-        // btnToggle.innerText = `Dark`
-        // btnToggle.className = `btn btn-dark`
-        localStorage.setItem("modoOscuro", false)
-    }
+    localStorage.setItem("modoOscuro", !isDarkStorageOn)
+    btnToggle.checked = !isDarkStorageOn
 })
